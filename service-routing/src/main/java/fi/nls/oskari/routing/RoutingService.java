9992930f620903c9be8707f709cbe4aee5633dbf
uhef <tuomas.jarvensivu@iki.fi>
2015-01-29 20:56:26 +0200

    Add ibatis support to routing service

--- service-routing/src/main/java/fi/nls/oskari/routing/RoutingService.java
+++ service-routing/src/main/java/fi/nls/oskari/routing/RoutingService.java
@@ -1,7 +1,46 @@
 package fi.nls.oskari.routing;
 
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
+import java.io.Reader;
+
 public class RoutingService {
+    private SqlMapClient client = null;
+    private static String SQL_MAP_LOCATION = "META-INF/SqlMapConfig.xml";
+
+    protected SqlMapClient getSqlMapClient() {
+        if (client != null) {
+            return client;
+        }
+
+        Reader reader = null;
+        try {
+            reader = Resources.getResourceAsReader(SQL_MAP_LOCATION);
+            client = SqlMapClientBuilder.buildSqlMapClient(reader);
+            return client;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to retrieve SQL client", e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
     public Long hevonen() {
-        return 42l;
+        try {
+            client = getSqlMapClient();
+            Long results = (Long)client.queryForObject("Routing.foo");
+            return results;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException("Failed to query", e);
+        }
     }
 }
